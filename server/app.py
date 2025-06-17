1f21a22cb7e14abaa56b7eef79ab780f657905e9
flfkirby <kirbyfred@gmail.com>
2025-06-17 15:44:18 +0100

    Maybe

--- server/app.py
+++ server/app.py
@@ -7,11 +7,6 @@ from flask_cors import CORS
 
 import os
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
-
-
 load_dotenv()
 app = Flask(__name__)
 CORS(app)
