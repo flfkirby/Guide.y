2b09c081e71438158d789a6c2e0fc8be531dcb10
flfkirby <kirbyfred@gmail.com>
2025-06-18 09:10:26 +0100

    Curl port 5000

--- server/app.py
+++ server/app.py
@@ -46,5 +46,5 @@ def route_summary():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Azure provides this env var
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
